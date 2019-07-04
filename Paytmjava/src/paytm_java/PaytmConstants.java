854e2338c1e2f8f85f31785a543a234596468d2e
VijayNandakumarkumar <vijay.nandakumar30@gmail.com>
2019-07-05 01:09:23 +0530

    Paytm test payment working.

---
+++ Paytmjava/src/paytm_java/PaytmConstants.java
@@ -0,0 +1,10 @@
+package paytm_java;
+public class PaytmConstants {
+  public final static String MID="bIsMxY42860847182298";
+  public final static String MERCHANT_KEY="cUnMAcRrP#USjC6K";
+  public final static String INDUSTRY_TYPE_ID="Retail";
+  public final static String CHANNEL_ID="WEB";
+  public final static String WEBSITE="worldpressplg";
+  public final static String PAYTM_URL="https://securegw-stage.paytm.in/theia/processTransaction"; 
+  
+}
